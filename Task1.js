c9cf7036a268fe4ab94b8964c443b8f6dc45065d
daryasen <130212460+daryasen@users.noreply.github.com>
2023-07-07 15:59:57 +0600

    Update and rename style.css to Task1.js

---
+++ Task1.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('myButton');
+const icon = document.getElementById('icon');
+
+button.addEventListener('click', function() {
+  icon.classList.toggle('icon_02');
+});
