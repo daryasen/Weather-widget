3397887a166276abfed2cf05fb8ab8e6c28219eb
daryasen <130212460+daryasen@users.noreply.github.com>
2023-07-27 19:36:17 +0600

    Delete Task1.js

--- Task1.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('myButton');
-const icon = document.getElementById('icon');
-
-button.addEventListener('click', function() {
-  icon.classList.toggle('icon_02');
-});
