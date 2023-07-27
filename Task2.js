364afcae6ff398f83f19ce2b3de5b8d4a1b2b739
daryasen <130212460+daryasen@users.noreply.github.com>
2023-07-27 19:35:57 +0600

    Delete Task2.js

--- Task2.js
+++
@@ -1,6 +0,0 @@
- function showScreenSize() {
-      var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-      var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-      var screenSizeText = 'Размер экрана: ' + screenWidth + ' x ' + screenHeight;
-      alert(screenSizeText);
- }
